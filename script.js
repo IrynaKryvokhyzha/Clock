28de08aa87cc5d132de08e23113746c712edf71e
Iryna Kryvokhyzha <95886613+IrynaKryvokhyzha@users.noreply.github.com>
2024-08-10 19:45:26 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const secondHand = document.querySelector(".second-hand");
+const minutesHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondDegrees = (seconds / 60) * 360 - 90;
+  secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+  //   console.log(seconds);
+  const minutes = now.getMinutes();
+  const minutesDegree = (minutes / 60) * 360 - 90;
+  minutesHand.style.transform = `rotate(${minutesDegree}deg)`;
+
+  const hour = now.getHours();
+  const hourDegree = (hour / 12) * 360 - 90;
+  hourHand.style.transform = `rotate(${hourDegree}deg)`;
+}
+
+setInterval(setDate, 1000);
